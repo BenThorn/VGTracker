2c52b2e299968e2f670832a37aaffa9361ebd3b8
BenThorn <brt4557@g.rit.edu>
2018-11-08 13:49:33 -0500

    test fix

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
